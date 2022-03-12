d3dac140137da73194d1b6788986f7adb66e0e57
satriyoaji27 <satriyoaji27@gmail.com>
2022-03-12 17:35:30 +0700

    add cors handler

--- backend/controllers/transporter-truck.controller.go
+++ backend/controllers/transporter-truck.controller.go
@@ -1,9 +1,12 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"hackathon-basic-backend/models"
+	"io"
 	"net/http"
+	"os"
 	"strconv"
 )
 
@@ -62,6 +65,76 @@ func UpdateTruck(c echo.Context) error {
 	plateType := c.FormValue("plate_type")
 	productionYear := c.FormValue("production_year")
 	status := c.FormValue("status")
+
+	// Multipart form
+	form, err := c.MultipartForm()
+	if err != nil {
+		return err
+	}
+	files := form.File["files"]
+	for _, file := range files {
+		// Source
+		src, err := file.Open()
+		if err != nil {
+			return err
+		}
+		defer src.Close()
+
+		// Destination
+		dst, err := os.Create(file.Filename)
+		if err != nil {
+			return err
+		}
+		defer dst.Close()
+
+		// Copy
+		if _, err = io.Copy(dst, src); err != nil {
+			return err
+		}
+	}
+
+	// Source
+	stnkFile, err := c.FormFile("stnk")
+	if err != nil {
+		return err
+	}
+	kirFile, err := c.FormFile("kir")
+	if err != nil {
+		return err
+	}
+
+	stnkSrc, err := stnkFile.Open()
+	if err != nil {
+		return err
+	}
+	defer stnkSrc.Close()
+
+	kirSrc, err := kirFile.Open()
+	if err != nil {
+		return err
+	}
+	defer kirSrc.Close()
+
+	// Destination
+	dst, err := os.Create(stnkFile.Filename)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+	dst, err = os.Create(kirFile.Filename)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	fmt.Println(kirSrc, stnkSrc)
+	// Copy
+	if _, err = io.Copy(dst, stnkSrc); err != nil {
+		return err
+	}
+	if _, err = io.Copy(dst, kirSrc); err != nil {
+		return err
+	}
 
 	result, err := models.Response{}, nil
 	if status == "Active" || status == "active" {
--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"hackathon-basic-backend/config"
 	"hackathon-basic-backend/models"
 	"hackathon-basic-backend/routes"
@@ -10,6 +12,9 @@ func main() {
 	models.Connect()
 
 	e := routes.Init()
-
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
 	e.Logger.Fatal(e.Start(":" + config.GoDotEnvVariable("PORT")))
 }
